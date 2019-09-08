304839cf191866dcd8853759746cd62cf51e6a79
Matt Beamish <matt.beamish@gsg.wa.edu.au>
2019-09-08 15:59:26 +0800

    Added jquery-modal

--- index.php
+++ index.php
@@ -10,8 +10,10 @@
   <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
   <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" type="text/css">
   <link rel="stylesheet" href="/node_modules/leaflet/dist/leaflet.css" type="text/css" />
+  <link rel="stylesheet" href="/node_modules/jquery-modal/jquery.modal.min.css" />
   <link rel="stylesheet" href="css/main.css" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Righteous|Roboto&display=swap" rel="stylesheet"> 
+
 
   
   <script src="https://kit.fontawesome.com/09ee5880ef.js"></script>
@@ -28,12 +30,12 @@
 
     </div>
   </div>
-    
+
   <script src="/node_modules/jquery/dist/jquery.min.js"></script>
   <script src="/node_modules/popper.js/dist/popper.min.js"></script>
   <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
   <script src="/node_modules/leaflet/dist/leaflet.js"></script>
-
+  <script src="/node_modules/jquery-modal/jquery.modal.min.js"></script>
   
   <script src="js/main.js"></script>
   </body>
